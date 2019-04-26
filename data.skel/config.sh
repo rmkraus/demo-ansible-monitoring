5bd8759d2f73121ae3855617c843c7c22776f3da
Ryan Kraus <rmkraus@gmail.com>
2019-04-26 16:43:11 -0400

    Added missing fields to the example config file.

--- data.skel/config.sh
+++ data.skel/config.sh
@@ -5,6 +5,7 @@ export DEMO_PREFIX="mydemo"
 export AWS_ACCESS_KEY_ID="ABCDEFG"
 export AWS_SECRET_ACCESS_KEY="ABCDEFG1234567HIJKLMNOP"
 export AWS_DEFAULT_REGION="us-east-1"
+export AWS_AVAILABILITY_ZONE="us-east-1a"
 export AWS_R53_ZONE_ID="ABCDEFG12345"
 export AWS_R53_DOMAIN="example.com"
 
@@ -30,10 +31,8 @@ export TF_LOG_PATH=/data/terraform.${DEMO_PREFIX}.log
 export TF_VAR_ami_id='ami-011b3ccf1bd6db744'  # RHEL 7.6
 export TF_VAR_app_instance_type="t2.small"
 export TF_VAR_app_node_count=3
+export TF_VAR_aws_availability_zone=${AWS_AVAILABILITY_ZONE}
 export TF_VAR_aws_r53_zone_id=${AWS_R53_ZONE_ID}
 export TF_VAR_demo_prefix="${DEMO_PREFIX}"
 export TF_VAR_tower_instance_type="t3.medium"
 export TF_VAR_zabbix_instance_type="t3.medium"
-
-# Setup Tower
-export TOWER_CONFIG_FILE="/data/tower_license.txt"
